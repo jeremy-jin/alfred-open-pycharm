21dca272d6f47468232d521d8244d563e0d14a67
Jeremy <12408427+jeremy-jin@users.noreply.github.com>
2022-04-22 18:25:29 +0800

    feat: first version (#1)

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# alfred-pycharm
+# Alfred-open-pycharm
+*** 
+***
+
+## What's *Alfred-open-pycharm*
+***
+**Alfred-open-pycharm** is a workflow plugin on Alfred, 
+which can help you to quickly open your favorite projects, 
+to improve development efficiency
+
+1.How to quickly open a recently opened project    
+2.How to quickly open a project that have not been recently opened    
+3.How to quickly switch project windows when multiple project windows are opened   
+4.How to quickly reveal the project or the folder in Finder instead
+
+## Installation
+***
+### Requirements
+You need Alfred 3.5+  
+
+This workflow needs to ensure that Pycharm's command line launcher is working:
+####Create command line launcher
+**Step Example**:  
+    1. Open The Pycharm App  
+    2. Go to Tools and Create Command-line Launcher, as follows:    
+    ![pycharm-alfred-workflow-create-cli](./doc/images/create_command_line_launcher.png)    
+    3. In the popup windows, just click on OK  
+    ![pycharm-alfred-workflow-create-cli](./doc/images/create_command_line_launcher_popup_windows.png) 
+    
+### Install
+1. Download workflow from `package` folder, or [here](./package/alfred-open-pycharm.alfredworkflow?raw=true)
+2. Double-click on downloaded file (alfred-open-pycharm.alfredworkflow)
+
+### How to use
+
---
+++ src/search.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+source ./workflow-utils.sh
+
+QUERY=$1
+LAUNCHER_DIR='/usr/local/bin/charm'
+
+# 检查文件是否存在且可读
+# 不存在或者不可读时，提示缺少文件，需要通过"jetbrains-alfred-workflow"创建文件
+if [ ! -r $LAUNCHER_DIR ]; then
+	noFoundCommandLine
+	exit
+fi
+
+# 获取Pycharm的信息
+
+# .e.g: ~/Library/Application Support/JetBrains/PyCharm2021.3
+CONFIG_PATH=$(grep "CONFIG_PATH =" $LAUNCHER_DIR | awk -F "'" '{print $2}')
+# .e.g: /Applications/PyCharm.app
+RUN_PATH=$(grep "RUN_PATH =" $LAUNCHER_DIR | awk -F "'" '{print $2}')
+# .e.g: /Applications/PyCharm.app/Contents/Info.plist
+APP_INFO_PATH=${RUN_PATH}'/Contents/Info.plist'
+VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$APP_INFO_PATH")
+VERSION=(${VERSION//./ })
+VERSION_YEAR=${VERSION[0]}
+VERSION_MONTH=${VERSION[1]}
+
+# 根据不同版本，获取信息位置不同
+if [ "$VERSION_YEAR" == "2019" ]; then
+	RECENT_PROJECTS_XML='/options/recentProjectDirectories.xml'
+	RECENT_XPATH=".//component[@name='RecentDirectoryProjectsManager']/option[@name='recentPaths']/list/option/@value"
+
+elif [ "$VERSION_YEAR" == "2020" ]; then
+  	if [ "$VERSION_MONTH" == "3" ]; then
+		RECENT_PROJECTS_XML='/options/recentProjects.xml'
+		RECENT_XPATH=".//component[@name='RecentProjectsManager']/option[@name='additionalInfo']/map/entry/@key"
+	else
+		RECENT_PROJECTS_XML='/options/recentProjectDirectories.xml'
+		RECENT_XPATH=".//component[@name='RecentProjectsManager']/option[@name='recentPaths']/list/option/@value"
+  fi
+
+elif [ "$VERSION_YEAR" == "2021" ]; then
+	RECENT_PROJECTS_XML='/options/recentProjects.xml'
+	RECENT_XPATH=".//component[@name='RecentProjectsManager']/option[@name='additionalInfo']/map/entry/@key"
+
+fi
+
+# 不支持的版本，跳出
+if [ ! $RECENT_PROJECTS_XML ]; then
+	unSupportVersion
+	exit
+fi
+
+# 获取最近打开的项目
+recent_projects=$(xmllint --xpath "${RECENT_XPATH}" "${CONFIG_PATH}${RECENT_PROJECTS_XML}")
+results=()
+
+# 遍历过滤最近打开的项目，得到过滤后的项目地址Array
+for project in $recent_projects
+do
+	project_path=$(echo "$project" | awk -v HOME="${HOME}" -F '"' '{sub(/\$USER_HOME\$/, HOME); print $2}')
+	if [ "$QUERY" ]; then
+		project_name=$(echo "$project_path" | awk -F '/' '{print $NF}')
+		match_result=$(echo "$project_name" | grep "${QUERY}")
+		if [ "$match_result" ]; then
+			results+=("$project_path")
+		fi
+	else
+		# 如果没有输入过滤字符串，直接添加结果列表中
+		results+=("$project_path")
+	fi
+done
+
+if [ ${results} ]; then
+	# 格式化result，生成Alfred需要的JSON数据
+	alfred_result=$(formatResult "${results[@]}")
+	echo "$alfred_result"
+	exit
+else
+	noProjectMatched
+	exit
+fi
