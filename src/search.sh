024c01b89b9a08bf6faafb79a87163601082d662
Jeremy <12408427+jeremy-jin@users.noreply.github.com>
2026-09-15 20:46:06 +0800

    feat: error messages

--- src/search.sh
+++ src/search.sh
@@ -26,13 +26,50 @@ if [ "$MAJOR_VERSION" == 2019 ] || [ "$MAJOR_VERSION" == 2020 ]; then
   fi
 fi
 
-RECENT_PROJECTS=$(getRecentProjects "$QUERY")
-if [ "${RECENT_PROJECTS}" ]; then
-  # 格式化result，生成Alfred需要的JSON数据
-  ALFRED_RESULT=$(formatResult "${RECENT_PROJECTS}")
-  echo "$ALFRED_RESULT"
-  exit
+# 调用函数
+expanded_path=$(is_path "$QUERY")
+ret=$?
+
+if [ $ret -eq 0 ]; then
+    # 输入是路径（~/xxx 或者 /xxx），expanded_path 是完整绝对路径
+    if [[ -d "$expanded_path" ]]; then
+        # 文件夹存在，直接返回Alfred item
+        echo '{"items": [
+        {
+            "uid": "",
+            "type": "",
+            "title": "Your Search exists",
+            "subtitle": "'"$QUERY"'",
+            "arg": "",
+            "autocomplete": "",
+        }]}'
+    else
+        # 路径格式合法，但文件夹不存在
+        echo '{"items": [
+        {
+            "uid": "",
+            "type": "",
+            "title": "Your Search does not exist",
+            "subtitle": "try another search",
+            "arg": "",
+            "icon": {
+                "path": "./warning.png"
+            },
+            "autocomplete": "",
+        }]}'
+    fi
 else
-  noProjectMatched
-  exit
+    # 普通关键词，执行mdfind搜索
+    # Search Recent Projects
+    RECENT_PROJECTS=$(getRecentProjects "$QUERY")
+    if [ "${RECENT_PROJECTS}" ]; then
+      # 格式化result，生成Alfred需要的JSON数据
+      ALFRED_RESULT=$(formatResult "${RECENT_PROJECTS}")
+      echo "$ALFRED_RESULT"
+      exit
+    else
+      noProjectMatched
+      exit
+    fi
 fi
+
